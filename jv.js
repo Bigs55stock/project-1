50d5ca3874c029a6df2cc8805fa9059d5b76cde0
humza15 <humzazafarullah@gmail.com>
2022-10-22 16:02:50 -0400

    some buttons and questions added

---
+++ jv.js
@@ -0,0 +1,24 @@
+const submit = document.querySelector(".submit")
+const reset = document.querySelector(".reset")
+const mainbox = document.querySelector("#mainbox")
+const question = document.querySelector(".question")
+const answers = document.querySelector(".answers")
+
+
+let questions10 = [
+    { Questions: "Who was the first president of the United States ?",
+      Choices: ["George Bush", "George Washington", "Abraham Lincoln", "John Cena"],
+      Answer: Choices[1],
+    },
+    {   Questions:"what is the largest ocean?",
+        Choices:["Atlantic Ocean", "Indian Ocean", "Pacific Ocean", "American Ocean"],
+        Answer: Choices[2],
+    },
+    {Questions:"How many boroughs are in NYC?",
+     Choices:["1", "10","26","5"],
+     Answer: Choices[3],
+    
+    },
+    {}
+
+]
